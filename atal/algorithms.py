02ec59ccfb42c029c71a69c8e83af9d3e93112f7
davidedup <david.pereira@ccc.ufcg.edu.br>
2018-10-09 11:23:20 -0300

    insertion sort

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,14 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
+	for index in range(1, len(alist)):
+
+		currentvalue = alist[index]
+		position = index
+
+		while position > 0 and alist[position-1] <= currentvalue:
+			alist[position] = alist[position-1]
+			position = position-1
+
+		alist[position] = currentvalue
 	return alist
